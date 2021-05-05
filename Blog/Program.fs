c772b184fbd01633e1ce75d4743f1981fec56d1c
Brian Berns <brianberns@gmail.com>
2021-05-05 13:42:01 -0400

    Add project files.

---
+++ Blog/Program.fs
@@ -0,0 +1,15 @@
+﻿namespace Blog
+
+open Statiq.App
+open Statiq.Web
+
+module Program =
+
+    [<EntryPoint>]
+    let main argv =
+        Bootstrapper
+            .Factory
+            .CreateWeb(argv)
+            .RunAsync()
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
