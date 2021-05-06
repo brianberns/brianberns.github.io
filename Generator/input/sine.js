07e6c824722a552672d66c3f22700332e28a0678
Brian Berns <brianberns@gmail.com>
2021-05-06 13:42:40 -0400

    Work on Graph class.

--- Generator/input/sine.js
+++ Generator/input/sine.js
@@ -1,52 +1,80 @@
-function showAxes(ctx) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    var xMin = 0;
-    ctx.beginPath();
-    ctx.strokeStyle = "rgb(128,128,128)";
-    // X-Axis
-    ctx.moveTo(xMin, height / 2);
-    ctx.lineTo(width, height / 2);
-    // Y-Axis
-    ctx.moveTo(width / 2, 0);
-    ctx.lineTo(width / 2, height);
-    // Starting line
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, height);
-    ctx.stroke();
-}
-function plotSine(ctx, xOffset) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "rgb(0,0,0)";
-    var x = 4;
-    var y = 0;
-    var amplitude = 40;
-    var frequency = 20;
-    ctx.moveTo(x, 50);
-    while (x < width) {
-        y = height / 2 + amplitude * Math.sin((x + xOffset) / frequency);
-        ctx.lineTo(x, y);
-        x++;
+var Graph = /** @class */ (function () {
+    function Graph(canvas) {
+        this.xStart = 0;
+        this.xZoom = 1;
+        this.yZoom = 1;
+        this.ctx = canvas.getContext("2d");
+        this.yStart = -canvas.height / 2;
     }
-    ctx.stroke();
-    ctx.save();
-}
-function draw() {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, 600, 600);
-    showAxes(context);
-    context.save();
-    plotSine(context, step);
-    context.restore();
-    step += 4;
-    window.requestAnimationFrame(draw);
-}
+    Graph.prototype.setStart = function (xStart, yStart) {
+        this.xStart = xStart;
+        this.yStart = yStart;
+    };
+    Graph.prototype.setZoom = function (xZoom, yZoom) {
+        this.xZoom = xZoom;
+        this.yZoom = yZoom;
+    };
+    Graph.prototype.setEnd = function (xEnd, yEnd) {
+        this.xZoom = this.ctx.canvas.width / (xEnd - this.xStart);
+        this.yZoom = this.ctx.canvas.height / (yEnd - this.yStart);
+    };
+    Graph.prototype.getPosition = function (xCoord, yCoord) {
+        var xPos = (xCoord - this.xStart) * this.xZoom;
+        var yPos = this.ctx.canvas.height - ((yCoord - this.yStart) * this.yZoom);
+        return [xPos, yPos];
+    };
+    Graph.prototype.getCoord = function (xPos, yPos) {
+        var xCoord = (xPos / this.xZoom) + this.xStart;
+        var yCoord = ((this.ctx.canvas.height - yPos) / this.yZoom) + this.yStart;
+        return [xCoord, yCoord];
+    };
+    Graph.prototype.moveToCoord = function (xCoord, yCoord) {
+        var _a = this.getPosition(xCoord, yCoord), xPos = _a[0], yPos = _a[1];
+        this.ctx.moveTo(xPos, yPos);
+    };
+    Graph.prototype.lineToCoord = function (xCoord, yCoord) {
+        var _a = this.getPosition(xCoord, yCoord), xPos = _a[0], yPos = _a[1];
+        this.ctx.lineTo(xPos, yPos);
+    };
+    Graph.prototype.drawAxes = function () {
+        this.ctx.strokeStyle = "gray";
+        this.ctx.beginPath();
+        var _a = this.getPosition(0, 0), xPos = _a[0], yPos = _a[1];
+        // x-axis
+        this.ctx.moveTo(0, yPos);
+        this.ctx.lineTo(this.ctx.canvas.width, yPos);
+        // y-axis
+        this.ctx.moveTo(xPos, 0);
+        this.ctx.lineTo(xPos, this.ctx.canvas.height);
+        this.ctx.stroke();
+    };
+    Graph.prototype.drawFunction = function (fun) {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.drawAxes();
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "red";
+        for (var xPos = 0; xPos < this.ctx.canvas.width; xPos++) {
+            var xCoord = this.getCoord(xPos, 0)[0];
+            var yCoord = fun(xCoord);
+            var _a = this.getPosition(0, yCoord), yPos = _a[1];
+            if (xPos == 0)
+                this.ctx.moveTo(xPos, yPos);
+            else
+                this.ctx.lineTo(xPos, yPos);
+        }
+        this.ctx.stroke();
+    };
+    return Graph;
+}());
 function init() {
+    var canvas = document.getElementById("canvas");
+    var graph = new Graph(canvas);
+    graph.setStart(-10, -1.5);
+    graph.setEnd(10, 1.5);
+    function draw() {
+        graph.drawFunction(Math.sin);
+        // window.requestAnimationFrame(draw);
+    }
     window.requestAnimationFrame(draw);
 }
-var step = -4;
 //# sourceMappingURL=sine.js.map
--- Generator/scripts/sine.ts
+++ Generator/scripts/sine.ts
@@ -1,66 +1,93 @@
-﻿
-function showAxes(ctx: CanvasRenderingContext2D) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    var xMin = 0;
+﻿class Graph {
 
-    ctx.beginPath();
-    ctx.strokeStyle = "rgb(128,128,128)";
-
-    // X-Axis
-    ctx.moveTo(xMin, height / 2);
-    ctx.lineTo(width, height / 2);
-
-    // Y-Axis
-    ctx.moveTo(width / 2, 0);
-    ctx.lineTo(width / 2, height);
-
-    // Starting line
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, height);
-
-    ctx.stroke();
-}
-
-function plotSine(ctx, xOffset) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "rgb(0,0,0)";
-
-    var x = 4;
-    var y = 0;
-    var amplitude = 40;
-    var frequency = 20;
-    ctx.moveTo(x, 50);
-    while (x < width) {
-        y = height / 2 + amplitude * Math.sin((x + xOffset) / frequency);
-        ctx.lineTo(x, y);
-        x++;
+    constructor(canvas: HTMLCanvasElement) {
+        this.ctx = canvas.getContext("2d");
+        this.yStart = -canvas.height / 2;
     }
-    ctx.stroke();
-    ctx.save();
-}
+    private readonly ctx: CanvasRenderingContext2D;
+    private xStart = 0;
+    private yStart: number;
+    private xZoom = 1;
+    private yZoom = 1;
 
-function draw() {
-    var canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    var context = canvas.getContext("2d");
+    public setStart(xStart: number, yStart: number) {
+        this.xStart = xStart;
+        this.yStart = yStart;
+    }
 
-    context.clearRect(0, 0, 600, 600);
-    showAxes(context);
-    context.save();
+    public setZoom(xZoom: number, yZoom: number) {
+        this.xZoom = xZoom;
+        this.yZoom = yZoom;
+    }
 
-    plotSine(context, step);
-    context.restore();
+    public setEnd(xEnd: number, yEnd: number) {
+        this.xZoom = this.ctx.canvas.width / (xEnd - this.xStart);
+        this.yZoom = this.ctx.canvas.height / (yEnd - this.yStart);
+    }
 
-    step += 4;
-    window.requestAnimationFrame(draw);
+    private getPosition(xCoord: number, yCoord: number): [number, number] {
+        let xPos = (xCoord - this.xStart) * this.xZoom;
+        let yPos = this.ctx.canvas.height - ((yCoord - this.yStart) * this.yZoom);
+        return [xPos, yPos];
+    }
+
+    private getCoord(xPos: number, yPos: number): [number, number] {
+        let xCoord = (xPos / this.xZoom) + this.xStart;
+        let yCoord = ((this.ctx.canvas.height - yPos) / this.yZoom) + this.yStart;
+        return [xCoord, yCoord];
+    }
+
+    private moveToCoord(xCoord: number, yCoord: number) {
+        let [xPos, yPos] = this.getPosition(xCoord, yCoord);
+        this.ctx.moveTo(xPos, yPos);
+    }
+
+    private lineToCoord(xCoord: number, yCoord: number) {
+        let [xPos, yPos] = this.getPosition(xCoord, yCoord);
+        this.ctx.lineTo(xPos, yPos);
+    }
+
+    private drawAxes() {
+        this.ctx.strokeStyle = "gray";
+        this.ctx.beginPath();
+        let [xPos, yPos] = this.getPosition(0, 0);
+
+            // x-axis
+        this.ctx.moveTo(0, yPos);
+        this.ctx.lineTo(this.ctx.canvas.width, yPos);
+
+            // y-axis
+        this.ctx.moveTo(xPos, 0);
+        this.ctx.lineTo(xPos, this.ctx.canvas.height);
+
+        this.ctx.stroke();
+    }
+
+    public drawFunction(fun: (x: number) => number) {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.drawAxes();
+
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "red";
+        for (let xPos = 0; xPos < this.ctx.canvas.width; xPos++) {
+            let [xCoord,] = this.getCoord(xPos, 0);
+            let yCoord = fun(xCoord);
+            let [, yPos] = this.getPosition(0, yCoord);
+            if (xPos == 0) this.ctx.moveTo(xPos, yPos)
+            else this.ctx.lineTo(xPos, yPos);
+        }
+        this.ctx.stroke();
+    }
 }
 
 function init() {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    let graph = new Graph(canvas);
+    graph.setStart(-10, -1.5);
+    graph.setEnd(10, 1.5);
+    function draw() {
+        graph.drawFunction(Math.sin);
+        // window.requestAnimationFrame(draw);
+    }
     window.requestAnimationFrame(draw);
 }
-
-var step = -4;
