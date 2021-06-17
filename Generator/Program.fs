c0a848216fdfb38cf4c539bf6411623cf45242a4
Brian Berns <brianberns@gmail.com>
2021-06-17 01:17:37 -0400

    Custom generator.

---
+++ Generator/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+open Markdig
+
+let markdownDirPath = @"..\..\..\..\Markdown"
+let htmlDirPath = @"..\..\..\..\docs"
+
+[<EntryPoint>]
+let main argv =
+    for mdFile in DirectoryInfo(markdownDirPath).EnumerateFiles() do
+        let md =
+            use rdr = new StreamReader(mdFile.FullName)
+            rdr.ReadToEnd()
+        let html = Markdown.ToHtml(md)
+        use wtr =
+            let htmlFileName =
+                $"{Path.GetFileNameWithoutExtension(mdFile.Name)}.html"
+            let htmlFilePath =
+                Path.Combine(htmlDirPath, htmlFileName)
+            new StreamWriter(htmlFilePath)
+        wtr.Write(html)
+    0
